0ac810269cc76a78d23ae7f1ca67f462ac130d2f
iambasantarai <iambasantarai@gmail.com>
2024-12-31 23:09:27 +0545

    Mimic terminal like appearance

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log("Hello, world!");
+import boxen from "boxen";
+import pc from "picocolors";
+
+const bold = pc.bold;
+const dimmy = pc.dim;
+const blueish = pc.blue;
+const cyanish = pc.cyan;
+const greyish = pc.gray;
+const greenish = pc.green;
+
+const content = `${greyish(new Date().toLocaleString())}\n${bold(dimmy("( ._.)"))}\n${bold(cyanish("basanta@void:~"))}${greenish("$")} cat info.json | jq\n${bold("{")}\n  ${bold(blueish('"name"'))}: ${greenish('"Basanta Rai"')},\n  ${bold(blueish('"about"'))}: ${greenish('"Software Developer"')},\n  ${bold(blueish('"github"'))}: ${greenish('"https://github.com/iambasantarai"')},\n  ${bold(blueish('"web"'))}: ${greenish('"https://www.basantarai.com.np"')}\n${bold("}")}`;
+
+console.log(
+  boxen(content, {
+    padding: 1,
+    borderStyle: "round",
+    dimBorder: true,
+    borderColor: "gray",
+  }),
+);
